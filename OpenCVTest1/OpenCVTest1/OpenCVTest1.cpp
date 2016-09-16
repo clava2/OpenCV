610e8505cfcb21a241a8ddca1a5b94adeab64a62
普通账户 <@DESKTOP-6F3A9O5>
2016-09-16 12:47:48 +0800

    黑白图像

--- OpenCVTest1/OpenCVTest1/OpenCVTest1.cpp
+++ OpenCVTest1/OpenCVTest1/OpenCVTest1.cpp
@@ -12,30 +12,40 @@ using std::endl;
 const int XR = 10;
 const int YR = 10;
 
+void showImage( int Value);
+
+Mat Input;
+Mat InputGray;
+Mat Output;
+
+int TrackValue1 = 3;
+int TrackValue2 = 0;
+
 int main(int argc, char* argv[])
 {
-
 	if (argc != 2)
 	{
-		cout << "输入的参数格式不正确！" << endl;
+		cout << "输入的参数不正确！" << endl;
 		return 1;
 	}
-	Mat Input = imread(argv[1]);
-	if (!Input.data)
-	{
-		cout << "加载失败！" << endl; 
-		return 2;
-	}
 
-	Mat Output = Input.clone();
-	medianBlur(Input, Output, XR);
+	Input = imread(argv[1]);
 
-	namedWindow("Original");
-	namedWindow("Target");
+	cvtColor(Input, InputGray, CV_BGR2GRAY);
 
-	imshow("Original", Input);
-	imshow("Target", Output);
+	namedWindow("Window1");
+
+	cvCreateTrackbar("Track1", "Window1", &TrackValue1,4,showImage);
+
+	cvCreateTrackbar("Track2", "Window1", &TrackValue2, 255, showImage);
+
+	showImage(0);
 
 	waitKey(0);
-	return 0;
+}
+
+void showImage(int Value)
+{
+	threshold(InputGray, Output, Value, 255, TrackValue1);
+	imshow("Window1", Output);
 }
